1bb7d9d42f81d283bda44c05519f5f80550b29bf
Dan <neveloffd@gmail.com>
2020-12-02 14:21:54 -0500

    progress bar working timer working

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,40 @@
-class tamagotchi {
-    constructor(name, health, mood){
+let sleepCount = 1;
+const sleepTimer = function sleepTimer() {
+  const updateSleepTime = function updateSleepTime() {
+    sleepCount++;
+    if(sleepCount >= 10){
+        clearInterval(timer);
+      }
+    /* if (sleepCount >= 10) {
+        clearInterval(timer); */ 
+        /* make timer stop at 10 */
+        /* add something here to call a function that will print Player died in his sleep */
+  };
+  const timer = setInterval(updateSleepTime, 60 * 1000);  
+}; 
+
+
+
+
+
+
+class Tamagotchi {
+    constructor(name){
         // 0 is optimal range for each stat, 10 being death/unhappy
-    let timer = new Timer();
     this.alive = true;
     this.name = name;
-    this.health = health;
-    this.mood = mood;
-    this.age = timer
-    this.image = imgurl;
+    this.health = 0;
+    this.mood = 0;
+    this.age = 1;
     }
+    updateAge(){
+        this.age += time
+    }
+
+    
+    
+      
+    
   
     changeImage(age){
     const $gotchi = $(".gotchi_img");
@@ -59,12 +85,29 @@ class tamagotchi {
     }
 
 }
+let value = 1;
+const $progress = $("progress");
+const setTimer = function setTimer(){
+    const updateTime = function updateTime() {
+        console.log("timer", time);
+        time++;
+        instance.age++
+        $progress.val(value);
+        value++
+        if (time === 600 * 1000) {
+            clearInterval(timer);
+            
+        }
+    }
+    const timer = setInterval(updateTime, 1000);
+};
+
+let time = 1
 
 
-  
-  
 
-const instance = new tamagotchi();
-const setUpGame = function setUpGame() {
-    const instance = new tamagotchi();
-    
+
+setTimer();
+const instance = new Tamagotchi("Jim");
+
+
